e7e1cce135db7345f6162b26c8ee4cab2bff509a
Florian Mair <f.mair@gmx.net>
2016-07-06 14:29:24 +0200

    intial commit without minifying and compression

---
+++ Gruntfile.js
@@ -0,0 +1,115 @@
+module.exports = function(grunt) {
+    grunt.initConfig({
+        responsive_images: {
+            rig: {
+
+                options: {
+                    engine: 'gm',
+                    newFilesOnly: 'false',
+                    sizes: [{
+                        name: 'small',
+                        width: '320',
+                        quality: 80
+                    }, {
+                        name: 'mid',
+                        width: '640px',
+                        quality: 80
+                    }, {
+                        name: 'large',
+                        width: '1280px',
+                        quality: 80
+                    }]
+                },
+                files: [{
+                    expand: true,
+                    src: ['*.{gif,jpg,png}'],
+                    cwd: 'imagesin/big/',
+                    dest: 'src/assets/img/'
+                }]
+
+            },
+            ris: {
+                options: {
+                    engine: 'gm',
+                    newFilesOnly: 'false',
+                    sizes: [{
+                        name: 'small',
+                        width: '320',
+                        quality: 80
+                    }, {
+                        name: 'mid',
+                        width: '360px',
+                        quality: 80
+                    }, {
+                        name: 'large',
+                        width: '720px',
+                        quality: 80
+                    }]
+
+                },
+                files: [{
+                    expand: true,
+                    src: ['*.{gif,jpg,png}'],
+                    cwd: 'imagesin/small/',
+                    dest: 'src/assets/img/'
+                }]
+
+            }
+        },
+        copy: {
+            main: {
+                files: [
+                    // includes files within path
+                    { expand: true, flatten: true, src: ['src/assets/img/*'], dest: 'dist/assets/img/', filter: 'isFile' }
+                ],
+            },
+        },
+        responsive_images_extender: {
+            complete: {
+                options: {
+                    separator: '@',
+                    baseDir: 'build',
+                    srcAttribute: 'smallest',
+                    sizes: [{
+                        selector: '.article-img',
+                        sizeList: [{
+                            cond: 'max-width: 30em',
+                            size: '100vw'
+                        }, {
+                            cond: 'max-width: 50em',
+                            size: '50vw'
+                        }, {
+                            cond: 'default',
+                            size: 'calc(33vw - 100px)'
+                        }]
+                    }]
+                },
+                files: [{
+                    expand: true,
+                    src: ['src/assets/pages/*.{html,htm,php}'],
+                    cwd: 'src/',
+                    dest: 'build/'
+                }]
+            }
+        }
+
+    });
+
+    grunt.loadNpmTasks('grunt-responsive-images');
+    grunt.loadNpmTasks('grunt-contrib-copy');
+
+    grunt.registerTask('img', function() {
+        grunt.task.run('responsive_images');
+        grunt.task.run('copy');
+    });
+
+    grunt.registerTask('imgsmall', function() {
+        grunt.task.run('responsive_images:ris');
+        grunt.task.run('copy');
+    });
+    grunt.registerTask('imgbig', function() {
+        grunt.task.run('responsive_images:rig');
+        grunt.task.run('copy');
+    });
+
+};
